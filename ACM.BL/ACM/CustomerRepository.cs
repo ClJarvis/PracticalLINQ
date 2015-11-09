e13d952a8e11913b46ef131a7f73dd84611ad99a
Chris_Jarvis <clj@ollure.com>
2015-11-09 01:01:52 -0600

    initial files

---
+++ ACM.BL/ACM/CustomerRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACM
+{
+    public class CustomerRepository
+    {
+        public Customer Find(List<Customer> customerList, int customerId)
+        {
+            Customer foundCustomer = null;
+
+            foreach (var c in customerList)
+            {
+                if (c.CustomerId == customerId)
+                {
+                    foundCustomer = c;
+                    break;
+                }
+            }
+            return foundCustomer;
+        }
+        public List<Customer> Retrieve()
+        {
+            return null; //stopped here
+        }
+    }
+}
